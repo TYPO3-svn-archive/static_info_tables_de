35bc748d5e898650b14e4bf49209e86b913a71b7
minderdl <minderdl@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-28 09:42:32 +0000

    Initial upload: TER version 2.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_de/trunk@71738 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "static_info_tables_de".
+#
+# Auto generated 03-10-2011 22:10
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Info Tables (de)',
+	'description' => '(de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '2.0.3',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.0-0.0.0',
+	'PHP_version' => '4.1.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'RenÃ© Fritz',
+	'author_email' => 'r.fritz@colorcube.de',
+	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
