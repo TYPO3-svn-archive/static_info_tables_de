70ffb748c58119e4ef53f2cdfe130488bf8055d1
minderdl <minderdl@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-25 17:38:40 +0000

    Upgrade data to static_info_tables 2.3.2 and other minor fixes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_de/trunk@83397 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "static_info_tables_de".
  *
- * Auto generated 22-03-2013 10:09
+ * Auto generated 25-01-2014 18:31
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '(de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '2.1.1',
+	'version' => '2.1.2',
 	'dependencies' => 'static_info_tables',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,15 +35,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'static_info_tables' => '2.3.0-',
+			'typo3' => '4.3.0-4.7.99',
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.0.0-6.0.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"36f4";s:20:"class.ext_update.php";s:4:"0db3";s:21:"ext_conf_template.txt";s:4:"7de5";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"096d";s:14:"ext_tables.php";s:4:"ef3a";s:14:"ext_tables.sql";s:4:"152a";s:28:"ext_tables_static_update.sql";s:4:"2192";s:16:"locallang_db.xml";s:4:"c00c";s:14:"doc/manual.sxw";s:4:"1d8d";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"39a1";s:20:"class.ext_update.php";s:4:"0db3";s:21:"ext_conf_template.txt";s:4:"7de5";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"096d";s:14:"ext_tables.php";s:4:"ef3a";s:14:"ext_tables.sql";s:4:"152a";s:28:"ext_tables_static_update.sql";s:4:"83be";s:16:"locallang_db.xml";s:4:"c00c";s:14:"doc/manual.sxw";s:4:"1d8d";}',
 	'suggests' => array(
 	),
 );
